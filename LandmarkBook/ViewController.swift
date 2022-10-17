dd94ce1f57f9b0296329323440acfcad36b60a4f
aliberkayberber <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>
2022-10-17 20:52:41 +0300

    Veri aktarma ve tableView bağlantısı eklendi

--- LandmarkBook/ViewController.swift
+++ LandmarkBook/ViewController.swift
@@ -10,6 +10,11 @@ import UIKit
 class ViewController: UIViewController,UITableViewDelegate , UITableViewDataSource {
 
     @IBOutlet weak var tableView: UITableView!
+    var landmarkNames = [String]()
+    var landmarkImages = [UIImage]()
+    
+    var chosenLandmarkName = ""
+    var chosenLandmarkImage = UIImage()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,19 +22,48 @@ class ViewController: UIViewController,UITableViewDelegate , UITableViewDataSour
         tableView.delegate = self
         tableView.dataSource = self
         
+        
+        landmarkNames.append("Manavgat")
+        landmarkNames.append("Izmir")
+        landmarkNames.append("Istanbul")
+        landmarkNames.append("Denizli")
+        landmarkNames.append("Paris")
+        
+        
+        landmarkImages.append(UIImage(named: "manavgat.jpeg")!)
+        landmarkImages.append(UIImage(named: "izmir.jpeg")!)
+        landmarkImages.append(UIImage(named: "istanbul.jpeg")!)
+        landmarkImages.append(UIImage(named: "denizli.jpeg")!)
+        landmarkImages.append(UIImage(named: "paris.jpeg")!)
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return landmarkNames.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
         var content = cell.defaultContentConfiguration()
-        content.text = "Table View"
-        content.secondaryText = "Test"
+        content.text = landmarkNames[indexPath.row]
+        
         cell.contentConfiguration = content
         return cell
     }
+
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        chosenLandmarkName = landmarkNames[indexPath.row]
+        chosenLandmarkImage = landmarkImages[indexPath.row]
+        performSegue(withIdentifier: "toDetailsVC", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toDetailsVC" {
+            let destinationVC = segue.destination as! DetailsVC
+            destinationVC.selectedLandmarkName = chosenLandmarkName
+            destinationVC.selectedLandmarkImage = chosenLandmarkImage
+            
+        }
+    }
+    
 }
 
